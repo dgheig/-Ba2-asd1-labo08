60926cac308cc903fcc881a65ada7762b6941bf8
David Gallay <davidtennis96@hotmail.com>
2020-05-12 21:55:05 +0200

    starting list version

--- tripartite_vector.h
+++ tripartite_vector.h
@@ -5,33 +5,30 @@
 #ifndef ASD1_LABS_2020_TRIPARTITE_H
 #define ASD1_LABS_2020_TRIPARTITE_H
 
-#include <list>
-#define CONTAINER std::list
-
-template <typename T>
-std::list<T> tripartite( std::list<T>& L )
+#include <vector>
+#define CONTAINER std::vector
 
 template <typename T>
 std::vector<T> tripartite( std::vector<T>& v )
 {
-    std::list<T> tmp;
-    auto it = L.begin();
+    std::vector<T> tmp;
+    tmp.reserve(v.size());
+    auto it = v.begin();
     auto cursor = it;
-    while(it != L.end()) {
-       auto previous = it++;
-       switch(*previous % 3) {
+    while(it != v.end()) {
+       switch(*it % 3) {
           case 0:
-             std::move(previous, it, std::back_inserter(tmp));
+             std::move(it, it + 1, std::back_inserter(tmp));
              break;
           case 1:
-            L.splice()
+            std::move(it, it + 1, cursor);
             ++cursor;
        }
+       ++it;
     }
-    L.erase(cursor, L.end());
+    v.erase(cursor, v.end());
 
     return tmp;
-
 }
 
 #endif //ASD1_LABS_2020_TRIPARTITE_H
